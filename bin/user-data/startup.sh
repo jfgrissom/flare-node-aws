fb573eca68bc35b6fb8bcc9fbf9300633c78f5e0
jfgrissom <jfgrissom@gmail.com>
2021-11-23 10:40:59 -0800

    Added avalanche repos to the host.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -21,21 +21,35 @@ apt -y install gcc g++ curl jq
 export REPO_ROOT=${HOME}/go
 export FLARENETWORK_ROOT=${REPO_ROOT}/src/gitlab.com/flarenetwork
 export FLARE_ROOT=${FLARENETWORK_ROOT}/flare
-export REPO_URL=https://gitlab.com/flarenetwork/flare.git
-export AVALANCHE_ROOT=${REPO_ROOT}/src/github.com/ava-labs/avalanchego
+export FLARE_REPO_URL=https://gitlab.com/flarenetwork/flare.git
+export AVALABS_ROOT=${REPO_ROOT}/src/github.com/ava-labs
+export AVALANCHE_ROOT=${AVALABS_ROOT}/avalanchego
+export AVALANCHE_REPO_URL=https://github.com:ava-labs/avalanchego.git
 export GOPATH=$(go env GOPATH)
 
 # Create all directories the repo depends on.
 mkdir -p ${FLARENETWORK_ROOT}
 mkdir -p ${AVALANCHE_ROOT}
 
-# Clone/Pull repo from gitlab which creates ${FLARE_ROOT}.
+# cd 
+# git clone 
+
+# Clone/Pull repo from gitlab for ${FLARE_ROOT}.
 cd ${FLARENETWORK_ROOT}
 if [ ! -d ${FLARE_ROOT} ]; then
-    git clone --no-checkout ${REPO_URL}
+    git clone --no-checkout ${FLARE_REPO_URL}
 else
     cd ${FLARE_ROOT}
-    git pull ${REPO_URL}
+    git pull ${FLARE_REPO_URL}
+fi
+
+# Clone/Pull repo from github for ${AVALANCHE_ROOT}
+cd ${AVALABS_ROOT}
+if [ ! -d ${AVALANCHE_ROOT} ]; then
+    git clone --no-checkout ${AVALANCHE_REPO_URL}
+else
+    cd ${AVALANCHE_ROOT}
+    git pull ${AVALANCHE_REPO_URL}
 fi
 
 cd ${FLARE_ROOT}
