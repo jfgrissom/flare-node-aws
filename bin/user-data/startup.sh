2e50414e0b713e9479895c9e15964272272894b7
jfgrissom <jfgrissom@gmail.com>
2021-11-23 14:33:29 -0800

    Adding GOPATH to root profile.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -15,9 +15,7 @@ snap install go --channel=1.15/stable --classic
 
 echo "Download and Installing NodeJS:"
 which node
-if [ ${?} -eq 1 ]; then
-  curl -sL https://deb.nodesource.com/setup_10.x | bash -
-fi
+if [ ${?} -eq 1 ]; then curl -sL https://deb.nodesource.com/setup_10.x | bash -; fi
 apt -y install nodejs
 npm install --global yarn
 
@@ -33,6 +31,11 @@ export FLARENETWORK_ROOT=${REPO_ROOT}/src/gitlab.com/flarenetwork
 export FLARE_ROOT=${FLARENETWORK_ROOT}/flare
 export FLARE_REPO_URL=https://gitlab.com/flarenetwork/flare.git
 export GOPATH=$(go env GOPATH)
+
+echo "Adding GOPATH to Root Profile:"
+export PROFILE=${HOME}/.profile
+grep "GOPATH" ${HOME}/.profile
+if [ $? -eq 1 ]; then echo "export GOPATH=${GOPATH}" >> ${PROFILE}; echo "" >> ${PROFILE}; fi
 
 echo "Creating Directories:"
 mkdir -p ${FLARENETWORK_ROOT}
