87eb899e575377da1045997ffbc6b0d9f46a17fd
jfgrissom <jfgrissom@gmail.com>
2021-11-23 08:39:59 -0800

    Removed spaces for env exports.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -4,13 +4,13 @@
 apt install git -y
 
 # Create a directory to hold repos.
-export REPO_ROOT = ${HOME}/Repos
+export REPO_ROOT=${HOME}/Repos
 mkdir -p ${REPO_ROOT}
 cd ${REPO_ROOT}
 
 # Clone/Pull repo from gitlab.
-export FLARE_ROOT = ${REPO_ROOT}/flare
-export REPO_URL = https://gitlab.com/flarenetwork/flare.git
+export FLARE_ROOT=${REPO_ROOT}/flare
+export REPO_URL=https://gitlab.com/flarenetwork/flare.git
 if [ ! -d ${FLARE_ROOT} ]; then
     git clone --no-checkout $URL ${FLARE_ROOT}
 else
