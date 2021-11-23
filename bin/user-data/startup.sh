4bb4382592e5f7351ee594f997a264819317ff15
jfgrissom <jfgrissom@gmail.com>
2021-11-23 08:36:14 -0800

    Added conditional to pull or clone repo depending on the state.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -1,14 +1,20 @@
 # Handle dependencies.
 apt install git -y
 
-# Create local directories.
-mkdir -p ${HOME}/Repos/
-cd ${HOME}/Repos
+# Create a directory to hold repos.
+REPO_ROOT = ${HOME}/Repos
+mkdir -p ${REPO_ROOT}
+cd ${REPO_ROOT}
 
-# Pull repo from gitlab.
-git clone --no-checkout https://gitlab.com/flarenetwork/flare.git
-
-# At this point ${HOME}/Repos/flare exists.
+# Clone/Pull repo from gitlab.
+FLARE_ROOT = ${REPO_ROOT}/flare
+REPO_URL = https://gitlab.com/flarenetwork/flare.git
+if [ ! -d ${FLARE_ROOT} ]; then
+    git clone --no-checkout $URL ${FLARE_ROOT}
+else
+    cd ${FLARE_ROOT}
+    git pull $URL
+fi
 
 # Install go environment.
 snap install go
