2c3d4c693a9f2b609e35fdccdf1b7b1dd5982c60
jfgrissom <jfgrissom@gmail.com>
2021-11-23 08:11:37 -0800

    Added git clone of flare node repo.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -1,2 +1,9 @@
 # Handle dependencies.
 apt install git -y
+
+# Create local directories.
+mkdir ${HOME}/Repos/
+cd ${HOME}/Repos
+
+# Pull repo from gitlab.
+git clone https://gitlab.com/flarenetwork/flare.git
