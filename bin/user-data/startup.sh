1d58c98318c348a0e0b9c5d63bdae03a519ff3dc
jfgrissom <jfgrissom@gmail.com>
2021-11-25 08:56:01 -0800

    Testing green checkbox.

--- bin/user-data/startup.sh
+++ bin/user-data/startup.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 # This scripts accepts 1 argument (local | songbird).
 
-echo "Installing Dependencies:"
+echo "✅ Installing Dependencies:"
 apt -y install git curl
 
-echo "Checking for installed version of Go:"
+echo " Checking for installed version of Go:"
 go version | grep 1.15
 if [ ${?} -eq 1 ]; then
     apt remove go
     apt remove golang-go
     snap remove go
 fi
-echo "Installing Go:"
+echo " Installing Go:"
 snap install go --channel=1.15/stable --classic
 
-echo "Checking for installed version of NodeJS:"
+echo " Checking for installed version of NodeJS:"
 node --version | grep v10
 if [ ${?} -eq 1 ]; then
     apt remove nodejs
 fi
-echo "Installing NodeJS:"
+echo " Installing NodeJS:"
 sudo snap install node --channel=10/stable --classic
 npm config set update-notifier false
 
-echo "Installing Yarn:"
+echo " Installing Yarn:"
 npm install --global yarn
 
-echo "Updating System:"
+echo " Updating System:"
 apt update
 
-echo "Completing Installation of Dependencies:"
+echo " Completing Installation of Dependencies:"
 apt -y install gcc g++ curl jq
 
-echo "Defineing Environment Variables:"
+echo " Defineing Environment Variables:"
 export REPO_ROOT=${HOME}/go
 export FLARENETWORK_ROOT=${REPO_ROOT}/src/gitlab.com/flarenetwork
 export FLARE_ROOT=${FLARENETWORK_ROOT}/flare
 export FLARE_REPO_URL=https://gitlab.com/flarenetwork/flare.git
 export GOPATH=$(go env GOPATH)
 
-echo "Adding GOPATH to Root Profile:"
+echo " Adding GOPATH to Root Profile:"
 export PROFILE=${HOME}/.profile
 grep "GOPATH" ${HOME}/.profile
 if [ $? -eq 1 ]; then echo "export GOPATH=${GOPATH}" >> ${PROFILE}; echo "" >> ${PROFILE}; fi
 
-echo "Creating Directories:"
+echo " Creating Directories:"
 mkdir -p ${FLARENETWORK_ROOT}
 
-echo "Cloning Repo:"
+echo " Cloning Repo:"
 cd ${FLARENETWORK_ROOT}
 if [ ! -d ${FLARE_ROOT} ]; then
     git clone --no-checkout ${FLARE_REPO_URL}
@@ -56,7 +56,7 @@ else
     git pull ${FLARE_REPO_URL}
 fi
 
-echo "Checking Out Master:"
+echo " Checking Out Master:"
 cd ${FLARE_ROOT}
 git checkout master
 
