a335f575889b7ac5d8d9d772fab9621bca5469cc
jfgrissom <jfgrissom@gmail.com>
2022-03-03 08:11:10 -0800

    Added logging dir.

--- bin/user-data/start-songbird.sh
+++ bin/user-data/start-songbird.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+source /home/flare/.profile
+
 cd ${HOME}/Repos/flare
 
-./build/flare --network-id=songbird \
+./build/flare \
+  --network-id=songbird \
   --http-host=0.0.0.0 \
   --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.ip")" \
   --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.nodeID")" \
-  > ${HOME}/logs/songbird.log &
+  --log-dir=${HOME/logs} &
