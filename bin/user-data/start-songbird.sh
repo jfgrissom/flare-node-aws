3d270a4a5f09cf48212a68b2434eb37b9fd7b431
jfgrissom <jfgrissom@gmail.com>
2022-03-03 08:18:41 -0800

    added console output log redirect.

--- bin/user-data/start-songbird.sh
+++ bin/user-data/start-songbird.sh
@@ -9,4 +9,5 @@ cd ${HOME}/Repos/flare
   --http-host=0.0.0.0 \
   --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.ip")" \
   --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.nodeID")" \
-  --log-dir=${HOME}/logs &
+  --log-dir=${HOME}/logs \
+  > ${HOME}/logs/console.log &
