fb4e911de92e8ff55e4a2b8c8ac9dec91623bda0
jfgrissom <jfgrissom@gmail.com>
2022-03-03 07:39:46 -0800

    addes script to startup songbird network.

---
+++ bin/user-data/start-songbird.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ${HOME}/repos/flare
+
+./build/flare --network-id=songbird \
+  --http-host=${PUBLIC_IP} \
+  --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.ip")" \
+  --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.nodeID")"
